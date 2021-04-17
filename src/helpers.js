513d78d60420309803572d886c757e435a46af1f
jrudio <justinjrudio@gmail.com>
2021-04-16 17:45:14 -0700

    get cpu info

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,44 @@
 const os = require('os');
 
 module.exports.getCPUs = () => {
+  let cpus = [];
 
+  let store = {};
+
+  os.cpus().forEach(cpu => {
+    if (store[cpu.model] !== undefined) {
+      store[cpu.model].cores++;
+
+      return;
+    }
+
+    store[cpu.model] = {
+      cores: 1,
+      speed: cpu.speed
+    };
+
+  });
+
+  for (let model in store) {
+    let cpu = store[model];
+
+    cpus.push({
+      cores: cpu.cores, // let's assume multithreading is enabled and just get the physical cores
+      // cores: cpu.cores / 2, // let's assume multithreading is enabled and just get the physical cores
+      speed: cpu.speed,
+      model
+    });
+  }
+
+  return cpus;
+};
+
+module.exports.convertToGhz = (n = 0) => {
+  n = n.toString();
+
+  n = n.slice(0, 3);
+
+  return `${n[0]}.${n[1]}${n[2]}`;
 };
 
 module.exports.getStorageDevices = () => {
--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
 const fs = require('fs');
+const path = require('path');
 const os = require('os');
 
 const {
   getCPUs,
   getMemory,
   getStorageDevices,
-  getNetworkInterfaces
+  getNetworkInterfaces,
+  convertToGhz
 } = require('./helpers');
 
-(() => {
+const getMachineDataAndSave = (outputPath = '') => {
+  let machineData = [];
+
   // get relevant info
+  let cpus = getCPUs();
 
-  // format into expected output
+  cpus = cpus.map(cpu => ({
+    Description: `${cpu.model} @ ${convertToGhz(cpu.speed)}GHz`,
+    NumberOfCores: cpu.cores
+  }));
 
-  // turn into JSON
+  // console.log(cpus);
+    // // format into expected output
+    // .then(data => {
+    //   console.log(data);
+    //   machineData.push(data)
 
-  // write to disk as machinedata.json
+    //   return machineData;
+    // })
+    // .then(allMachineData => {
+    //   // turn into JSON
+    //   let output = null;
 
-})();
+    //   try {
+    //     output = JSON.stringify(allMachineData, null, '\t');
+    //   } catch (err) {
+    //     throw err;
+    //   }
+
+    //   return output;
+    // })
+    // // write to disk as machinedata.json
+    // .then(allMachineDataStr => fs.writeFileSync(outputPath, allMachineDataStr))
+    // .catch(err => console.log(`getting machine data and saving failed: ${err}`));
+};
+
+const outputPath = path.join(__dirname, 'machinedata.json');
+
+getMachineDataAndSave(outputPath);
